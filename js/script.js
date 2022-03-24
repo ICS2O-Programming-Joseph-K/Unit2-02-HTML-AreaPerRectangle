4bb254ef4bca0ce5d9d28ffaa1552b9a9cb70c04
Joseph Kwon <99274059+JosephK2006@users.noreply.github.com>
2022-03-24 15:03:57 +0000

    Added JS and css

---
+++ js/script.js
@@ -0,0 +1,6 @@
+
+function doMathClicked () {
+  // this function does basic math
+  document.getElementById('multiply-math').innerHTML = ' The area is ' + (5 * 3) + 'cm^2'
+  document.getElementById('add-math').innerHTML = ' The perimeter is ' + (5 + 3 + 5 + 3) + 'cm'
+}
